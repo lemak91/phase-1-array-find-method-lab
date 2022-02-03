24256f7c0d53d187902a3d28ef8401fa089f7648
johnlemak <lemak91@mac.com>
2022-02-03 10:37:49 -0600

    commit tree c2a96bb362284d0fbe34cff9a130d2dd0f3f86c4

--- index.js
+++ index.js
@@ -1,1 +1,56 @@
 // code your solution here
+
+// write function called suberbowlWin()
+function suberbowlWin(record) {
+    
+}
+
+
+// the function should recieve 1 parameter, an array of javascript objects
+// each opbject has two properties: year and result
+// ex: { year: 1997, results: 'W' }
+// it should use find() to test each Object to see if the result is "w" - a win!
+    // tools i might use:
+        // record.results
+        // conditional (if or ternary)
+        // test each Object => loop ?
+// it should return the year when the win occurred (if it ocured at all!)
+// if no win is found, it should return, sadly, undefined
+
+// input:
+    // data type: number
+    // example: 7
+    const record = [
+        { year: "2015", result: "W"},
+        { year: "2014", result: "N/A"},
+        { year: "2013", result: "L"},
+        //...
+      ]
+
+// output
+      //data type: boolean
+      // example: true
+
+// 7 -> true
+// 8 -> false
+// 9 -> true
+// ....
+
+function superbowlWin(record){
+
+
+
+    function isOdd(element) {
+         return (element % 2 === 1);
+    }
+
+    function isWin(element, index, array){
+        return (element.result === 'W');
+    }
+
+    const result = record.find(isWin)
+
+    if (result === undefined) {
+        return undefined;
+    }
+}
